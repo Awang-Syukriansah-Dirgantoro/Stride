7b878afe0d75bed54cb414e1da93ac400af6cde9
Awang-Syukriansah-Dirgantoro <awangsyukriansahdirgantoro@gmail.com>
2023-08-01 19:30:28 +0700

    Initial Commit

---
+++ Stride Watch App/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride Watch App
+//
+//  Created by Awang Syukriansah Dirgantoro on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Stride_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Awang Syukriansah Dirgantoro on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
